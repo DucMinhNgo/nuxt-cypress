18289e0df93bac04710c24563c316348fda09d4f
ducngo <duc.ngo@salto.vn>
2024-05-13 16:47:15 +0700

    feat: cypress install crypto-js

---
+++ cypress/component/Crypto.cy.js
@@ -0,0 +1,17 @@
+import CryptoJS from 'crypto-js';
+
+describe('Crypto', () => {
+    it('crypto', async () => {
+        // Encrypt
+        const ciphertext = CryptoJS.AES.encrypt('tintin message', 'secret key 123').toString();
+    
+        // Decrypt
+        const bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret key 123');
+        const originalText = bytes.toString(CryptoJS.enc.Utf8);
+    
+        console.log({ciphertext});
+        console.log({bytes});
+        console.log({originalText});
+        console.log({originalText}); // 'my message'
+      })
+});
